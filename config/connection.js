e6402d78dea21f88f9ff94d98649c0362026a58d
Jessica Gertler <jessicadgertler@gmail.com>
2017-09-10 19:40:52 -0600

    added heroku/jaws option

--- config/connection.js
+++ config/connection.js
@@ -5,15 +5,19 @@
 // // Require mysql
 var mysql = require("mysql");
 
+//setting up jaws for heroku deployment
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
 // // Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: 'burgers_db'
-});
-
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: 'burgers_db'
+  });
+};
 // // Connect to the database
 connection.connect(function(err) {
   if (err) {
